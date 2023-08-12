94594ff208b6eb77d941836150c2a56ab90ef75d
Kelloggs13 <tobias@byland.info>
2023-08-12 19:03:38 +0200

    seem to have a working version...

--- app.py
+++ app.py
@@ -54,10 +54,67 @@ def read_data(file):
     st.warning("Uploaded file must be either .csv or .xlsx")
   return df
 
+def preprocess_data(df):
+  # Add the target label and pop target-column 
+  df["target"] = df[select_target]
+  df.drop(select_target, axis = 1, inplace = True)
+  first_column = df.pop('target')
+  df.insert(0, 'target', first_column)
+
+  # split X, y   
+  X = df.drop("target", axis=1).copy()
+  y = df["target"].copy() 
+  
+  # make target numeric binary  
+  lb = preprocessing.LabelBinarizer()
+  y = lb.fit_transform(y)
+  
+  # encode  character model features
+  vars_categorical = X.select_dtypes(include="O").columns.to_list()
+  vars_remainder = X.select_dtypes(exclude="O").columns.to_list()
+  ct = ColumnTransformer([("encoder", OrdinalEncoder(), vars_categorical)],remainder="passthrough",)
+  ct.fit(X)
+  X = ct.transform(X)
+  X = pd.DataFrame(X, columns=vars_categorical+vars_remainder)
+  
+  return X, y
+
+def fit_eval_model(model):
+  mod = model
+  mod_str = str(mod)
+  mod.fit(X_train, y_train)
+  importance = mod.feature_importances_
+  importance = pd.Series(importance, index=X_train.columns).sort_values(ascending = False)
+  importance = pd.DataFrame(importance)
+  importance["feature"] = importance.index
+  importance.columns = ["feature_importance", "feature"]
+  y_train_pred = mod.predict(X_train)
+  y_test_pred = mod.predict(X_test)
+    
+  acc_train = metrics.accuracy_score(y_train, y_train_pred)
+  f1_train =  metrics.f1_score(y_train, y_train_pred, average = "binary")
+  auc_train = metrics.roc_auc_score(y_train, y_train_pred, average = "macro")
+  
+  acc_test = metrics.accuracy_score(y_test, y_test_pred)
+  f1_test =  metrics.f1_score(y_test, y_test_pred, average = "binary")
+  auc_test = metrics.roc_auc_score(y_test, y_test_pred, average = "macro")
+  df_eval = pd.DataFrame({
+      ' ':['Train','Test','Change'],
+      'Accuracy':[acc_train, acc_test, (acc_test/acc_train)-1],
+      'F1':[f1_train, f1_test, (f1_test/f1_train)-1],
+      'AUC':[auc_train, auc_test, (auc_test/auc_train)-1]
+    })
+  conf_matrix = confusion_matrix(y_test, y_test_pred)
+    
+  return mod_str, df_eval, importance, conf_matrix, y_test_pred, mod
+      
+      
+      
 st.write("1. Upload Data File (CSV or XLSX)")
 input_data = st.file_uploader(' ')
 
 if input_data is not None:
+  
   df_input = read_data(input_data)
   
   st.write("2. Choose Target for Classification Model")
@@ -67,24 +124,8 @@ if input_data is not None:
   
   if select_target is not None: # ---- PRE-PROCESSING ----
     
-    # Add the target label and pop target-column 
-    df_input["target"] = df_input[select_target]
-    df_input.drop(select_target, axis = 1, inplace = True)
-    first_column = df_input.pop('target')
-    df_input.insert(0, 'target', first_column)
-    
-    X = df_input.drop("target", axis=1).copy()
-    y = df_input["target"].copy() 
-    lb = preprocessing.LabelBinarizer()
-    y = lb.fit_transform(y)
-    
-    # encode all remaining character variables
-    vars_categorical = X.select_dtypes(include="O").columns.to_list()
-    vars_remainder = X.select_dtypes(exclude="O").columns.to_list()
-    ct = ColumnTransformer([("encoder", OrdinalEncoder(), vars_categorical)],remainder="passthrough",)
-    ct.fit(X)
-    X = ct.transform(X)
-    X = pd.DataFrame(X, columns=vars_categorical+vars_remainder)
+    # pre-process
+    X, y = preprocess_data(df_input)
     
     # Split data into train and test
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=.7, random_state=25)
@@ -100,36 +141,8 @@ if input_data is not None:
     select_mode = st.selectbox(' ', [None, "Fitting", "Scoring"])
 
     if select_mode == "Fitting":
+      
       st.header("Fitted Models")
-        
-      def fit_eval_model(model):
-        mod = model
-        mod_str = str(mod)
-        mod.fit(X_train, y_train)
-        importance = mod.feature_importances_
-        importance = pd.Series(importance, index=X_train.columns).sort_values(ascending = False)
-        importance = pd.DataFrame(importance)
-        importance["feature"] = importance.index
-        importance.columns = ["feature_importance", "feature"]
-        y_train_pred = mod.predict(X_train)
-        y_test_pred = mod.predict(X_test)
-        
-        acc_train = metrics.accuracy_score(y_train, y_train_pred)
-        f1_train =  metrics.f1_score(y_train, y_train_pred, average = "binary")
-        auc_train = metrics.roc_auc_score(y_train, y_train_pred, average = "macro")
-    
-        acc_test = metrics.accuracy_score(y_test, y_test_pred)
-        f1_test =  metrics.f1_score(y_test, y_test_pred, average = "binary")
-        auc_test = metrics.roc_auc_score(y_test, y_test_pred, average = "macro")
-        df_eval = pd.DataFrame({
-          ' ':['Train','Test','Change'],
-          'Accuracy':[acc_train, acc_test, (acc_test/acc_train)-1],
-          'F1':[f1_train, f1_test, (f1_test/f1_train)-1],
-          'AUC':[auc_train, auc_test, (auc_test/auc_train)-1]
-        })
-        conf_matrix = confusion_matrix(y_test, y_test_pred)
-        
-        return mod_str, df_eval, importance, conf_matrix, y_test_pred, mod
        
       col_l, col_m, col_r = st.columns([1, 1, 1])
       
@@ -157,48 +170,29 @@ if input_data is not None:
         st.download_button("Download Model", data=pickle.dumps(fm_rforest[5]),file_name=f"{fm_rforest[0]}.pkl")
 
     if select_mode == "Scoring":
-      # Streamlit app title
-      st.title("Model Scoring and Confusion Matrix")
       
       # Upload the pickled model
       model_file = st.file_uploader("Upload Pickled Model", type=["pkl"])
       
-      # Upload the input data (Excel or CSV)
-      input_data_file = st.file_uploader("Upload Input Data (Excel or CSV)", type=["csv", "xlsx"])
+      if model_file is not None:
+        
+        # Load the pickled model
+        with model_file as f:
+          model_scoring = pickle.load(f)
+    
+        # Display uploaded input data
+        st.write("Uploaded Data for Scoring:")
+        st.write(df_input)
       
-      if model_file and input_data_file:
-          try:
-              # Load the pickled model
-              with model_file as f:
-                  model = pickle.load(f)
+        # Model scoring
+        y_scored = model_scoring.predict(X)
+    
+        # Calculate confusion matrix
+        cm = confusion_matrix(y, y_scored)
+    
+        # Display confusion matrix
+        st.write("Confusion Matrix:")
+        st.write(pd.DataFrame(cm, columns=["Predicted 0", "Predicted 1"], index=["Actual 0", "Actual 1"]))
       
-              # Load the input data
-              input_data = pd.read_csv(input_data_file) if input_data_file.name.endswith(".csv") else pd.read_excel(input_data_file)
-      
-              # Display uploaded input data
-              st.write("Uploaded Input Data:")
-              st.write(input_data)
-      
-              # Select target variable
-              target_variable = st.selectbox("Select Target Variable", input_data.columns)
-      
-              # Split data into features and target
-              X = input_data.drop(target_variable, axis=1)
-              y = input_data[target_variable]
-      
-              # Train-test split for scoring
-              X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-      
-              # Model scoring
-              y_pred = model.predict(X_test)
-      
-              # Calculate confusion matrix
-              cm = confusion_matrix(y_test, y_pred)
-      
-              # Display confusion matrix
-              st.write("Confusion Matrix:")
-              st.write(pd.DataFrame(cm, columns=["Predicted 0", "Predicted 1"], index=["Actual 0", "Actual 1"]))
-      
-          except Exception as e:
-              st.error("An error occurred: " + str(e))
+
   
